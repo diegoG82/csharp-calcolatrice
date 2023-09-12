3dc24d7f5425664f72a74fe5bc06ef24475ea764
diegoG82 <diegogiardina@hotmail.it>
2023-09-12 16:20:14 +0200

    Refactioring generale e inserimento valore minimo e massimo

--- csharp-calcolatrice/CalcoliHelper.cs
+++ csharp-calcolatrice/CalcoliHelper.cs
@@ -37,17 +37,61 @@
             return a * b;
         }
 
-        //Valore assoluto numero intero
+        //Valore assoluto numero intero 
         public static int ValoreAssoluto(int a)
         {
-            return Math.Abs(a);
+            if (a < 0)
+            {
+                return a * -1;
+            }
+            else
+            {
+                return a;
+            }
+
+            //return Math.Abs(a)
+
         }
 
         //Valore Assoluto numero double
         public static double ValoreAssoluto(double a)
         {
-            return Math.Abs(a);
+            //return Math.Abs(a);
+
+            if (a < 0)
+            {
+                return a * -1;
+            }
+            else
+            {
+                return a;
+            }
         }
+
+        //Minimo tra due numeri interi
+        public static int ValoreMinimo(int a, int b)
+        {
+            return Math.Min(a, b);
+        }
+
+        //Minimo tra due numeri double
+        public static double ValoreMinimo(double c, double d)
+        {
+            return Math.Min(c, d);
+        }
+
+        //Massimo tra due numeri interi
+        public static int ValoreMassimo(int a, int b)
+        {
+            return Math.Max(a, b);
+        }
+
+        //Massimo tra due numeri double
+        public static double ValoreMassimo(double c, double d)
+        {
+            return Math.Max(c, d);
+        }
+
     }
 
 }
--- csharp-calcolatrice/Program.cs
+++ csharp-calcolatrice/Program.cs
@@ -1,6 +1,15 @@
 ﻿
 using csharp_calcolatrice;
 
+//DICHIARAZIONE VALORI INT E DOUBLE CHE USEREMO NELLE NOSTRE FUNZIONI
+
+int a = 5;
+int b = 6;
+double c = 7.5;
+double d = 6.5;
+
+//CICLO WHILE PER SCEGLIERE L'OPERAZIONE DA EFFETTUARE
+
 while (true)
 {
     Console.WriteLine("Quale operazione vuoi fare?");
@@ -11,7 +20,11 @@ while (true)
     Console.WriteLine("5 - Moltiplicazione di numeri interi");
     Console.WriteLine("6 - Moltiplicazione di numeri double");
     Console.WriteLine("7 - Valore assoluto di un numero intero");
-    Console.WriteLine("8 -Valore assoluto di un numero double");
+    Console.WriteLine("8 - Valore assoluto di un numero double");
+    Console.WriteLine("9 - Valore minimo tra due  numeri interi");
+    Console.WriteLine("10 - Valore minimo tra due numeri double");
+    Console.WriteLine("11 - Valore massimo tra due  numeri interi");
+    Console.WriteLine("12 - Valore massimo tra due numeri double");
     Console.WriteLine("Esci dal programma");
 
     string scelta = Console.ReadLine();
@@ -24,90 +37,75 @@ while (true)
 
     if (scelta == "1")
     {
-        int a = 5;
-        int b = 6;
-
         int risultato = CalcoliHelper.Somma(a, b);
-
         Console.WriteLine($"La somma di {a} e {b} e' : {risultato}");
-        break;
     }
 
     if (scelta == "2")
     {
-        double a = 5.5;
-        double b = 6.5;
-
-        double risultato = CalcoliHelper.Somma(a, b);
-
-        Console.WriteLine($"La somma di {a} e {b} e' : {risultato}");
-        break;
+        double risultato = CalcoliHelper.Somma(c, d);
+        Console.WriteLine($"La somma di {c} e {d} e' : {risultato}");
     }
 
     if (scelta == "3")
     {
-        int a = 6;
-        int b = 5;
-
         int risultato = CalcoliHelper.Sottrazione(a, b);
-
         Console.WriteLine($"La sottrazione di {a} e {b} e' : {risultato}");
-        break;
     }
 
     if (scelta == "4")
     {
-        double a = 6.5;
-        double b = 5.5;
-
-        double risultato = CalcoliHelper.Sottrazione(a, b);
-
-        Console.WriteLine($"La sottrazione di {a} e {b} e' : {risultato}");
-        break;
+        double risultato = CalcoliHelper.Sottrazione(c, d);
+        Console.WriteLine($"La sottrazione di {c} e {d} e' : {risultato}");
     }
 
     if (scelta == "5")
     {
-        int a = 6;
-        int b = 5;
-
         int risultato = CalcoliHelper.Moltiplicazione(a, b);
-
         Console.WriteLine($"La moltiplicazione di {a} e {b} e' : {risultato}");
-        break;
     }
 
     if (scelta == "6")
     {
-        double a = 5.5;
-        double b = 6.5;
-
-        double risultato = CalcoliHelper.Moltiplicazione(a, b);
-
-        Console.WriteLine($"La moltiplicazione di {a} e {b} e' : {risultato}");
-        break;
+        double risultato = CalcoliHelper.Moltiplicazione(c, d);
+        Console.WriteLine($"La moltiplicazione di {c} e {d} e' : {risultato}");
     }
 
     if (scelta == "7")
     {
-        int a = 7;
-
         int risultato = CalcoliHelper.ValoreAssoluto(a);
-
         Console.WriteLine($"Il valore assoluto di {a} e' : {risultato}");
-        break;
     }
 
     if (scelta == "8")
     {
-        double a = 7.5;
-
-        double risultato = CalcoliHelper.ValoreAssoluto(a);
-
-        Console.WriteLine($"Il valore assoluto di {a} e' : {risultato}");
-        break;
+        double risultato = CalcoliHelper.ValoreAssoluto(c);
+        Console.WriteLine($"Il valore assoluto di {c} e' : {risultato}");
     }
 
+    if (scelta == "9")
+    {
+        int risultato = CalcoliHelper.ValoreMinimo(a, b);
+        Console.WriteLine($"Il minimo tra {a} e {b} e' : {risultato}");
+    }
+
+    if (scelta == "10")
+    {
+        double risultato = CalcoliHelper.ValoreMinimo(c, d);
+        Console.WriteLine($"il valore minimo tra {c} e {d} e' : {risultato}");
+    }
+
+    if (scelta == "11")
+    {
+        int risultato = CalcoliHelper.ValoreMinimo(a, b);
+        Console.WriteLine($"Il minimo massimo tra {a} e {b} e' : {risultato}");
+    }
+
+    if (scelta == "12")
+    {
+        double risultato = CalcoliHelper.ValoreMinimo(c, d);
+        Console.WriteLine($"il valore massimo tra {c} e {d} e' : {risultato}");
+    }
 
 
     if (scelta == "Esci")
