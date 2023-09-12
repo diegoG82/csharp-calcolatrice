f80909e5a3ac08ea8543be58bc0bbe5230360efb
diegoG82 <diegogiardina@hotmail.it>
2023-09-12 14:42:11 +0200

    Aggiungere i file di progetto.

---
+++ csharp-calcolatrice/CalcoliHelper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_calcolatrice
+{
+
+    public static class CalcoliHelper
+    {
+        //Somma:
+        public static int Somma(int a, int b)
+        {
+            return a + b;
+        }
+    }
+
+}
---
+++ csharp-calcolatrice/Program.cs
@@ -0,0 +1,39 @@
+﻿
+using csharp_calcolatrice;
+
+while (true)
+{
+    Console.WriteLine("Quale operazione vuoi fare?");
+    Console.WriteLine("1 - Somma di numeri interi");
+    Console.WriteLine("Esci dal programma");
+
+    string scelta = Console.ReadLine();
+
+    if (string.IsNullOrEmpty(scelta))
+    {
+        Console.WriteLine("Seleziona un opzione valida");
+        continue;
+    }
+
+    if (scelta == "1")
+    {
+        int a = 5;
+        int b = 6;
+
+        int risultato = CalcoliHelper.Somma(a, b);
+
+        Console.WriteLine($"La somma di {a} e {b} e' : {risultato}");
+        break;
+    }
+
+    if (scelta == "Esci")
+    {
+        break;
+    }
+
+}
+
+
+
+
+
